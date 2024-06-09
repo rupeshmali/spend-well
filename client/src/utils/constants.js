c30e6d7d85cd71a79f4a673f77b10169aec6270c
Rupesh Mali <rupeshmali670@gmail.com>
2024-06-09 16:12:56 +0530

    client: initial commit, react router setup

---
+++ client/src/utils/constants.js
@@ -0,0 +1,10 @@
+export const PATHS = {
+    LOGIN: '/login',
+    REGISTER: '/register',
+    HOME: '/home',
+    LANDING: '/'
+}
+
+export const SERVER_URL = 'http://localhost:3000'
+
+export const TOKEN_KEY = 'access_token'
